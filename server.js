c65b88ca345229a55aa2f6ee785444960af0a2de
Jorge Betancur <jbetancur@elespectador.com>
2020-11-06 03:11:04 +0000

    Added schema validation

--- server.js
+++ server.js
@@ -1,39 +1,54 @@
-'use strict'
+'use strict';
 
-const path = require('path')
+const path = require('path');
 const fastify = require('fastify')({
   logger: {
-    prettyPrint: true
-  }
-})
-const port = process.env.PORT || 3000
+    prettyPrint: true,
+  },
+});
+const port = process.env.PORT || 3000;
+
+const schema = {
+  querystring: {
+    type: 'object',
+    required: ['course'],
+    properties: {
+      name: {
+        type: 'string',
+      },
+      course: {
+        type: 'string',
+      },
+    },
+  },
+};
 
 fastify.register(require('point-of-view'), {
   engine: {
-    handlebars: require('handlebars')
-  }
-})
+    handlebars: require('handlebars'),
+  },
+});
 
 fastify.register(require('fastify-static'), {
   root: path.join(__dirname, 'public'),
-  prefix: '/public/'
-})
+  prefix: '/public/',
+});
 
 // Challenge: NO SCHEMA????
 // Please add a schema to validate the following properties from query string:
 //  - name
 //  - course (required)
-fastify.get('/', (request, reply) => {
-  const name = request.query.name || 'Anonymous'
-  const course = request.query.course
-  reply.view('/templates/index.hbs', { name, course })
-})
+fastify.get('/', { schema }, (request, reply) => {
+  const name = request.query.name || 'Anonymous';
+  const course = request.query.course;
+  reply.view('/templates/index.hbs', { name, course });
+});
 
-async function start () {
-  await fastify.listen(port)
+async function start() {
+  await fastify.listen(port);
 }
 
-start().catch(err => {
-  fastify.log.error(err)
-  process.exit(1)
-})
+start().catch((err) => {
+  fastify.log.error(err);
+  process.exit(1);
+});
